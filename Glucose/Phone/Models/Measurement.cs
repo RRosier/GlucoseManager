431328e247a2513c6fa27b3dffde11bbcd60fd84
Ronald Rosier <ronald_rosier@yahoo.co.uk>
2014-01-12 17:57:30 +0100

    Add first implementation of MontList page

---
+++ Glucose/Phone/Models/Measurement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosier.Glucose.Model
+{
+    /// <summary>
+    /// Represents a single glucose measurement.
+    /// </summary>
+    public class Measurement
+    {
+        /// <summary>
+        /// Date and time when the value is taken.
+        /// </summary>
+        public DateTimeOffset DateTime { get; set; }
+        /// <summary>
+        /// The measured glucose value.
+        /// </summary>
+        public int GlucoseValue { get; set; }
+        /// <summary>
+        /// Amount of insuline units taken.
+        /// </summary>
+        public int InsulineUnits { get; set; }
+        /// <summary>
+        /// Free comments about the measurement.
+        /// </summary>
+        public string Comments { get; set; }
+    }
+}
---
+++ Glucose/Phone/Models/MonthSummary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rosier.Glucose.Model
+{
+    /// <summary>
+    /// Summary class for a single month.
+    /// </summary>
+    public class MonthSummary
+    {
+        /// <summary>
+        /// Gets or sets the month.
+        /// </summary>
+        /// <value>
+        /// The month.
+        /// </value>
+        public string Month { get; set; }
+        /// <summary>
+        /// Gets or sets the total measures for this month.
+        /// </summary>
+        /// <value>
+        /// The total measures.
+        /// </value>
+        public int TotalMeasures { get; set; }
+        /// <summary>
+        /// Gets or sets the total insuline taken for this month.
+        /// </summary>
+        /// <value>
+        /// The total insuline.
+        /// </value>
+        public int TotalInsuline { get; set; }
+        /// <summary>
+        /// Gets or sets the total glucose measured for this month.
+        /// </summary>
+        /// <value>
+        /// The total glucose.
+        /// </value>
+        public int TotalGlucose { get; set; }
+
+        /// <summary>
+        /// Gets the average insuline intake for this month.
+        /// </summary>
+        /// <value>
+        /// The average insuline.
+        /// </value>
+        public int AverageInsuline { get { return TotalInsuline / TotalMeasures; } }
+        /// <summary>
+        /// Gets the average glucose measured for this month.
+        /// </summary>
+        /// <value>
+        /// The average glucose.
+        /// </value>
+        public int AverageGlucose { get { return TotalGlucose / TotalMeasures; } }
+    }
+}
